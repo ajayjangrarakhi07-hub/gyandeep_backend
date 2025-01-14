0d31404c5f7a106b2aadeeb211d18cde5a434748
ajay-jangra <ajayjangrarakhi@gmail.com>
2025-01-14 23:28:02 +0530

    review1

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { createReview, getReviews } = require('../controllers/reviewController');
+const { createReview, getReviews } = require('../controllers/reviewController');  // Import getReviews
 
 const router = express.Router();
 
 // Route to create a new review
 router.post('/reviews', createReview);
- 
+
 // Route to fetch all reviews
-router.get('/reviews', getReviews);  
+router.get('/reviews', getReviews);  // This should work now
 module.exports = router;
