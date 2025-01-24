a3230431051572e2b0b0a16ec8b64ef632fafac0
ajay-jangra <ajayjangrarakhi@gmail.com>
2025-01-25 00:10:57 +0530

    update

--- routes/fullMockTestRoutes.js
+++ routes/fullMockTestRoutes.js
@@ -8,12 +8,12 @@ const {
 const router = express.Router();
 
 // Route to fetch all test subject names
-router.get('/test-subjects', getAllTestSubjects);
+router.get('/mock-test-subjects', getAllTestSubjects);
 
 // Route to fetch test details by subject name
-router.get('/test/:testSubjectName', getTestBySubjectName);
+router.get('/mock-test/:testSubjectName', getTestBySubjectName);
 
 // Admin route to add a new test
-router.post('/add-test', addNewTest);
+router.post('/add-mock-test', addNewTest);
 
 module.exports = router;
