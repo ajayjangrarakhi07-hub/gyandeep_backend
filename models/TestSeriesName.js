3c381b9ba33628886cefb85d9e09b83f6a5511f8
ajay-jangra <ajayjangrarakhi@gmail.com>
2025-02-03 17:09:44 +0530

    testTimeDuration

--- models/TestSeriesName.js
+++ models/TestSeriesName.js
@@ -1,9 +1,15 @@
-// models/TestSeriesName.js
 const mongoose = require('mongoose');
 
 const testSeriesSchema = new mongoose.Schema({
     testSeriesName: { type: String, required: true },
-    image: { type: String, default: 'https://res.cloudinary.com/deujqrznl/image/upload/v1737561942/36-512_hhg5ri.webp' }
+    image: {
+        type: String,
+        default: 'https://res.cloudinary.com/deujqrznl/image/upload/v1737561942/36-512_hhg5ri.webp'
+    },
+    testTimeDuration: {
+        type: Number, // Store duration in seconds (e.g., 7200 for 2 hours)
+        required: true
+    }
 });
 
 module.exports = mongoose.model('TestSeriesName', testSeriesSchema);
