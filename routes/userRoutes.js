dee5a44fbb230d6974697b8ce14a593466f54d13
ajay-jangra <ajayjangrarakhi@gmail.com>
2026-03-01 23:23:39 +0530

    try 2 solving error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, checkDevice } = require("../controllers/userController");
+const { registerUser, checkDevice, verifyPaidUser } = require("../controllers/userController");
 const deviceCheck = require("../middleware/deviceCheck");
 
 router.post("/check-device", checkDevice);
